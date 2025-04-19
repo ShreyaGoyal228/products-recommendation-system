1533b09c3d256d41f939a0e1b20ee46e449dac0c
Shreya Goyal <goyalshreya228@Gmail.com>
2025-04-19 22:47:41 +0530

    add : frontend-files

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect,useState } from "react";
+import axios from "axios";
+import React from "react";
+import ProductCard from "./product-card";
 function App() {
+  const [products,setProducts]=useState([]);
+  const fetchProducts = async () => {
+    const allProducts = await axios.get("/api/products");
+    setProducts(allProducts.data);
+    console.log("products are", allProducts.data);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>
+    {products.map((item)=>(
+      <ProductCard key={item.id} product={item}/>
+    ))}
+  </div>
+
+  )
 }
 
 export default App;
